3f106f47500e4fc276a9cf671939bf5a22967bc1
rohin <rohinmaleywar@gmail.com>
2022-04-23 12:40:14 +0530

    commit

---
+++ src/main/java/com/SecondServletSecondProject.java
@@ -0,0 +1,21 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test4")
+public class SecondServletSecondProject extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out=resp.getWriter();
+		out.println("<h1>This is Second Servlet..You can access by Foreign Request Dispatcher</h1>");
+	}
+}
